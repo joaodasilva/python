db74096a0a285b434061e96630e83782d6f8a526
Joao da Silva <silvajoao@gmail.com>
2012-10-06 15:19:47 +0200

    Call assign during input parsing

--- solve.py
+++ solve.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# 1. Load the input to memory
-
 import sys
 
 digits = '123456789'
@@ -10,6 +8,18 @@ indices = [(row, col) for row in range(9) for col in range(9)]
 def group(iterable, times):
   return zip(*[iter(iterable)] * times)
 
+def assign(values, index, value):
+  values[index] = value
+  return True
+
+def parse_grid(grid):
+  values = dict((s, digits) for s in indices)
+  grid = dict(zip(indices, (c for c in grid if c in digits or c in '0.')))
+  assert len(grid) == 81
+  if not all(assign(values, s, d) for s, d in grid.items() if d in digits):
+    return False
+  return values
+
 def pretty_print(values):
   width = max(len(s) for s in values.values())
   items = [values[(row, col)].center(width) for row, col in indices ]
@@ -17,10 +27,6 @@ def pretty_print(values):
   grid = [' | '.join(s) for s in group(rows, 3)]
   sep = '\n' + '-+-'.join(['-'.join(['-' * width] * 3)] * 3) + '\n'
   print sep.join(['\n'.join(l) for l in group(grid, 3)])
-
-def parse_grid(grid):
-  return dict(zip(indices, (c if c in digits else digits
-                            for c in grid if c in digits or c in '0.')))
 
 if __name__ == '__main__':
   pretty_print(parse_grid(open(sys.argv[1]).read()))
