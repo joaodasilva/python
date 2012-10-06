db302776aad9dd4e296dd405ec4cb52c03463e8a
Joao da Silva <silvajoao@gmail.com>
2012-10-06 16:53:34 +0200

    Added remove, checking for contradictions and propagating removes

--- solve.py
+++ solve.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python
 
+from itertools import *
 import sys
-
-digits = '123456789'
-indices = [(row, col) for row in range(9) for col in range(9)]
 
 def group(iterable, times):
   return zip(*[iter(iterable)] * times)
 
-def assign(values, index, value):
-  values[index] = value
+digits = '123456789'
+indices = [(row, col) for row in range(9) for col in range(9)]
+
+unitlist = ( group(indices, 9) +                        # rows
+             group([(y, x) for x, y in indices], 9) +   # columns
+             # squares
+             [ tuple(product(rows, cols)) for rows in group(range(9), 3)
+                                          for cols in group(range(9), 3) ] )
+
+units = dict((s, filter(lambda unit: s in unit, unitlist)) for s in indices)
+
+peers = dict((s, set(x for u in units[s] for x in u if x!=s)) for s in indices)
+
+def remove(values, index, value):
+  if value not in values[index]:
+    # Already removed. Prevent infinite recursion between 2 squares with one
+    # value each.
+    return True
+  values[index] = values[index].replace(value, '')
+  if len(values[index]) == 0:
+    # Removed all values.
+    return False
+  elif len(values[index]) == 1:
+    # Single value: remove it from the peers of |index|.
+    if not all(remove(values, p, values[index]) for p in peers[index]):
+      return False
   return True
+
+def assign(values, index, value):
+  # Invalid if any peer of |index| already has |value|.
+  return all(remove(values, index, o) for o in values[index].replace(value, ''))
 
 def parse_grid(grid):
   values = dict((s, digits) for s in indices)
@@ -28,5 +54,8 @@ def pretty_print(values):
   sep = '\n' + '-+-'.join(['-'.join(['-' * width] * 3)] * 3) + '\n'
   print sep.join(['\n'.join(l) for l in group(grid, 3)])
 
-if __name__ == '__main__':
-  pretty_print(parse_grid(open(sys.argv[1]).read()))
+def solve(puzzle):
+  pretty_print(parse_grid(puzzle))
+
+if __name__ == '__main__' and len(sys.argv) == 2:
+  solve(open(sys.argv[1]).read())
